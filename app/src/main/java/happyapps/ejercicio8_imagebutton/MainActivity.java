4bc255ce0e556cf24284e562b8bd74a92400c0d6
Longynus <hacpin@hotmail.com>
2016-06-30 01:17:55 -0500

    Probado y funcionando sin problemas

---
+++ app/src/main/java/happyapps/ejercicio8_imagebutton/MainActivity.java
@@ -0,0 +1,26 @@
+package happyapps.ejercicio8_imagebutton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tv1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv1=(TextView)findViewById(R.id.tv1);
+    }
+
+
+    public void llamar(View view){
+        tv1.setText("Llamando");
+    }
+}
